8f9cac9cfe28ab4dc9e84a9f8512670d134337ab
saima-salar <saimasalar198@gmail.com>
2024-12-16 03:15:17 +0500

    edit

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,30 +3,33 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <section className="w-1440 h-1000 flex flex-col md:flex-row items-center justify-between px-6 md:px-10 py-16 bg-[#FBEBB5]">
-    {/* Left Section */}
-    <div className="text-center md:text-left max-w-md space-y-6 md:ml-12 mt-8">
-      <h1 className="text-[46px] font-bold leading-tight animate-fadeIn">
-        Rocket single <br className="hidden md:block" />
-        <span className="mt-2 inline-block">Seater</span>
-      </h1>
+<section className="w-full max-w-screen-xl h-auto flex flex-col md:flex-row gap-10 items-center justify-between px-6 md:px-10 py-16 bg-[#FBEBB5]">
+  {/* Left Section */}
+  <div className="text-center md:text-left max-w-md space-y-6 md:ml-12 mt-8">
+    <h1 className="text-[64px] font-medium leading-tight animate-fadeIn flex md:space-x-2 justify-center md:justify-start">
+      <span className="inline-block">Rocket Single</span>
+      <span className="inline-block">Seater</span>
+    </h1>
+
+    <button className="inline-block px-6 py-2 mt-4 text-black font-semibold border-b-2 border-black hover:text-gray-700">
+      Shop Now
+    </button>
+  </div>
+
+  {/* Right Section */}
+  <div className="mt-10 md:mt-0 mr-0">
+    <Image
+      src="/images/Rocket single seater 1.png"
+      alt="Rocket single seater sofa"
+      className="w-full h-auto max-w-[853px] transform transition duration-700 ease-in-out hover:scale-105 hover:rotate-1"
+      width={853}
+      height={1000}
+    />
+  </div>
+</section>
+
   
-      <button className="inline-block px-6 py-2 mt-4 text-black font-semibold border-b-2 border-black hover:text-gray-700">
-        Shop Now
-      </button>
-    </div>
   
-    {/* Right Section */}
-    <div className="mt-10 md:mt-0">
-      <Image
-        src="/images/Rocket single seater 1.png"
-        alt="Rocket single seater sofa"
-        className="w-full h-100 max-w-full md:max-w-xl transform transition duration-700 ease-in-out hover:scale-105 hover:rotate-1"
-        width={500} // Example fixed width for larger screens
-        height={400} // Example fixed height to maintain aspect ratio
-      />
-    </div>
-  </section>
   
   
   );
